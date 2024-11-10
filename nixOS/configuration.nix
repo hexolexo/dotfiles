ecc63df48b3ebb52cc1c0eefb1ad09d4d6e006c4
hexolexo <hexolexo132@proton.me>
2024-11-11 08:43:00 +1100

    Removed dnsproxy2 because the school blocks it

--- nixOS/configuration.nix
+++ nixOS/configuration.nix
@@ -16,8 +16,8 @@ in
 
 	# Networking
 	networking = {
-		networkmanager.dns = "none";
-		nameservers = [ "127.0.0.1" "::1" ];
+		#networkmanager.dns = "none"; # Unset DNS
+		#nameservers = [ "127.0.0.1" "::1" ]; # Set DNS to Locally hosted
 		hostName = "hexolexo"; # Define your hostname.
 		firewall = {
     		enable = true;
@@ -28,29 +28,29 @@ in
 	};
 
 	# DNS over HTTPS
-	services.dnscrypt-proxy2 = {
-		enable = true;
-		settings = {
-	    	ipv6_servers = true;
-	    	require_dnssec = true;
+#	services.dnscrypt-proxy2 = {
+#		enable = true;
+#		settings = {
+#	    	ipv6_servers = true;
+#	    	require_dnssec = true;
+#	
+#	    	sources.public-resolvers = {
+#	        	urls = [
+#	        		"https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+#	          		"https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+#	        	];
+#	      	cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+#	      	minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+#	      	};
+#	
+#	      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+#	      server_names = [ "cs-sydney" "adfilter-syd" "dnscry.pt-sydney-ipv6" ];
+#	    };
+#	  };
 	
-	    	sources.public-resolvers = {
-	        	urls = [
-	        		"https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-	          		"https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-	        	];
-	      	cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-	      	minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-	      	};
-	
-	      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-	      server_names = [ "cs-sydney" "adfilter-syd" "dnscry.pt-sydney-ipv6" ];
-	    };
-	  };
-	
-	systemd.services.dnscrypt-proxy2.serviceConfig = {
-		StateDirectory = "dnscrypt-proxy";
-	};
+#	systemd.services.dnscrypt-proxy2.serviceConfig = {
+#		StateDirectory = "dnscrypt-proxy";
+#	};
 
   # Set your time zone.
   time.timeZone = "Australia/Sydney";
